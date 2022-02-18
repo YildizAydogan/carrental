6adf00470523dafc3ede7a0ffc4110a00bbf29d5
Yildiz Aydogan <94450275+YildizAydogan@users.noreply.github.com>
2022-02-18 22:50:04 +0100

    Initial Commit

--- src/main/java/com/lecture/carrental/repository/UserRepository.java
+++ src/main/java/com/lecture/carrental/repository/UserRepository.java
@@ -3,11 +3,18 @@ package com.lecture.carrental.repository;
 
 import com.lecture.carrental.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+  @Repository
+  @Transactional  //bir hata aldigimizda geri dönmek icinde kullanilir islem geri alma düzeltme vs rollback ozelligi var
+  public interface UserRepository extends JpaRepository<User, Long> {
 
 
+   // @Query("SELECT u from User u where u.email = ?1")
+     Optional<User>findByEmail(String email);
 
 }
---
+++ src/main/java/com/lecture/carrental/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.lecture.carrental.security.service;
+
+
+import com.lecture.carrental.domain.User;
+import com.lecture.carrental.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       User user=userRepository.findByEmail(email)
+               .orElseThrow(()->new UsernameNotFoundException("User not found with Email: "+ email));
+
+
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+}
